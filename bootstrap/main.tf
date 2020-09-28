e72b4c7c53a3f10871fa5ef1367df2e8e05b7473
jfarnell-test <johnbfarnell@gmail.com>
2020-09-28 16:00:51 -0400

    added bootstrap

---
+++ bootstrap/main.tf
@@ -0,0 +1,33 @@
+# TFE_HOSTNAME
+# TFE_TOKEN
+provider "tfe" {}
+
+# Create a new TFE Organization for testing
+resource "random_pet" "org_name" {}
+
+/* resource "tfe_organization" "org" {
+  name  = random_pet.org_name.id
+  email = format("%s@email.com", random_pet.org_name.id)
+} */
+
+# Create an "admin" team
+resource "tfe_team" "admin" {
+  organization = "test-org"
+  name         = "admin-team"
+}
+
+# Create an "dev" team
+resource "tfe_team" "dev" {
+  organization = "test-org"
+  name         = "dev-team"
+}
+
+output "tfe" {
+  value = {
+    organization_name = "test-org"
+    teams = {
+      admin = tfe_team.admin.id
+      dev   = tfe_team.dev.id
+    }
+  }
+}
