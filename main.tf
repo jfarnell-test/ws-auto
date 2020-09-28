2c199cf6aa61e6ceccd7697d463b13406e2cf29a
jfarnell-test <johnbfarnell@gmail.com>
2020-09-28 16:33:20 -0400

    add team ids

---
+++ main.tf
@@ -0,0 +1,37 @@
+#TFE_HOSTNAME
+#TFE_TOKEN
+
+provider "tfe" {}
+
+#new module call for each workspace set
+
+module "workspace" {
+  source = "./modules/workspaces"
+
+  organization_name = var.organization_name
+  names             = ["workspace_a_tf12-1", "workspace_a_tf12-2", "workspace_a_tf12-3"]
+  teams_access = [
+    {
+      id     = var.team_admin
+      access = "admin"
+    },
+    {
+      id     = var.team_dev
+      access = "plan"
+    }
+  ]
+}
+
+module "workspace" {
+  source = "./modules/workspaces"
+
+  organization_name = var.organization_name
+  names             = ["workspace_b_tf12-1", "workspace_b_tf12-2"]
+  teams_access = [
+    {
+      id     = var.team_dev
+      access = "plan"
+    }
+  ]
+}
+
